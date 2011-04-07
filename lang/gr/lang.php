a6f26e2974d45537d807067a1e6a9a62102cae91
Constantinos Xanthopoulos <conx@xanthopoulos.info>
2011-04-07 05:19:38 +0300

    Greek translation for lang.php notifymail.txt optinmail.txt

---
+++ lang/gr/lang.php
@@ -0,0 +1,64 @@
+<?php
+
+$lang['menu'] = 'Διαχείριση Blogs (BlogTNG)';
+
+$lang['entry_latest']   = 'Τελευταίες %d Εγγραφές';
+$lang['comment_latest'] = 'Τελευταία %d Σχόλια';
+$lang['numhits'] = 'Number of Hits'; #FIXME
+
+$lang['blog']    = 'Blog';
+$lang['author']  = 'Συγγραφέας';
+$lang['dashboard'] = 'Πίνακας ελέγχου'; # FIXME check for better translation
+$lang['entry']   = 'Εγγραφή';
+$lang['filter']  = 'Φίλτρο';
+$lang['tags']    = 'Ετικέτες';
+$lang['commentstatus'] = 'Τα σχόλια είναι';
+
+$lang['searchresults']  = 'Αποτελέσματα αναζήτησης';
+
+$lang['comments']    = 'Σχόλια';
+$lang['0comments']   = '%d Σχόλια';
+$lang['1comments']   = '%d Σχόλιο';
+$lang['Xcomments']   = '%d Σχόλια';
+$lang['readmore']    = 'Διαβάστε περισσότερα…';
+
+$lang['prev'] = '&lt; Νεότερα άρθρα';
+$lang['next'] = 'Παλαιότερα άρθρα &gt;';
+
+$lang['title']   = 'Τίτλος:';
+$lang['create']  = 'Δημιούργησε';
+$lang['created'] = 'Ημερομηνία Δημιουργίας';
+
+$lang['js']['reply'] = 'Απαντήστε σε αυτό το σχόλιο';
+
+$lang['comment_mail']      = 'E-Mail';
+$lang['comment_avatar']    = 'Εικονίδιο';
+$lang['comment_name']      = 'Όνομα';
+$lang['comment_web']       = 'Ιστοσελίδα';
+$lang['comment_preview']   = 'Προεπισκόπηση';
+$lang['comment_submit']    = 'Σχόλιο';
+$lang['comment_source']    = 'Πηγή';
+$lang['comment_status']    = 'Κατάσταση';
+$lang['comment_text']      = 'Κείμενο';
+$lang['comment_ip']        = 'IP Διεύθυνση';
+$lang['comment_subscribe'] = 'Εγγραφείτε στα σχόλια'; # FIXME chech operation
+
+$lang['subscr_subject'] = 'Νέο σχόλιο στο %s';
+$lang['optin_subject']  = 'Παρακαλείστε να επιβεβαιώσετε την εγγραφή σας'; # FIXME maybe s/εγγραφή/συνδρομή/
+
+$lang['optin_ok']  = 'Σε ευχαριστώ. Τώρα θα λαμβάνεις τα νέα σχόλια μέσω email';
+$lang['optin_err'] = 'Ουπς. Το κλειδί αυτό δεν βρέθηκε. Μήπως συνέβει ένα σφάλμα κατά την αντιγραφή/επικόλληση;';
+
+$lang['unsubscribe_ok'] = 'Η διαγραφή σας ήταν επιτυχής. Δεν θα λαμβάνεται πλέον ενημερώσεις για νέα σχόλια σε αυτό το άρθρο.';
+$lang['unsubscribe_err_key'] = 'Ουπς. Το κλειδί αυτό δεν βρέθηκε. Μήπως συνέβει ένα σφάλμα κατά την αντιγραφή/επικόλληση;';
+$lang['unsubscribe_err_other'] = 'Ουπς. Η εγγραφή σας δεν βρέθηκε. Μήπως συνέβει ένα σφάλμα κατά την αντιγραφή/επικόλληση;'; #FIXME maybe s/εγγραφή/συνδρομή
+
+$lang['btn_comment_edit']  = 'Επεξεργασία';
+$lang['act_comment_edit']  = 'Επεξεργασία σχολίου';
+
+$lang['msg_comment_save']           = 'Η αλλαγές στο σχόλιο αποθηκεύτηκαν';
+$lang['msg_comment_status_change']  = 'Η κατάσταση του σχολίου άλλαξε';
+$lang['msg_comment_delete']         = 'Το σχόλιο διαγράφηκε';
+$lang['msg_entry_blog_change']      = 'Η εγγραφή στο Blog άλλαξε';
+
+$lang['err_notitle'] = 'Δεν δόθηκε τίτλος άρθρου';
